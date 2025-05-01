68873127c898f55bf91dc693c895b1520e3971d5
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2025-05-01 16:54:54 -0400

    Funcionalidades listas

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 const verDetallesButton = document.getElementById('verDetalles');
 const volverButton = document.getElementById('volver');
 const tarjeta = document.querySelector('.tarjeta');
+//Elementos de la tarjeta
+const tarjetaFrontal = document.getElementById('frontal');
+const tarjetaTrasera = document.getElementById('posterior');
 // Función para voltear la tarjeta
+function voltearTarjeta() {
+    tarjetaFrontal.style.transform = 'rotateY(180deg)';
+    tarjetaTrasera.style.transform = 'rotateY(0deg)';
+}
 // Función para volver a la cara frontal
+function volverTarjeta() {
+    tarjetaFrontal.style.transform = 'rotateY(0deg)';
+    tarjetaTrasera.style.transform = 'rotateY(180deg)';
+}
+// Evento para mostrar detalles
+verDetallesButton.addEventListener('click', () => {
+    voltearTarjeta();
+});
+volverButton.addEventListener('click', () => {
+    volverTarjeta();
+});
